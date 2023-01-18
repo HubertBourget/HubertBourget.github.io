e9a9b80b3b30b4114dd555f32fcb0e0d96902b6f
Hubert Bourget <hubbourget@hotmail.com>
2023-01-17 22:30:50 -0600

    segment auth test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,9 +33,9 @@ const RESOURCES = {
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "78433a25a8ef0cb00f211fae0881f806",
-"/": "78433a25a8ef0cb00f211fae0881f806",
-"main.dart.js": "800d47d959e7b2c9bb4f4befe286b400",
+"index.html": "78c966a626676969d7fd076b85b42ee4",
+"/": "78c966a626676969d7fd076b85b42ee4",
+"main.dart.js": "ab06211a7294b24d1a7175817b404bc5",
 "manifest.json": "536d812d8f7317a2b531e39f18d02b93",
 "version.json": "82d8c4e9d8ebd3e6e1a374e412e6e065"
 };
